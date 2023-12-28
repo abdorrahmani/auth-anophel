ca8341ba1d5ec5a60799672ecb0afa18f6d8eaf6
MohammadAbdorrahmani <mohammad02abdorrahmani@gmail.com>
2023-12-28 15:38:16 +0330

    final

--- functions/functions.php
+++ functions/functions.php
@@ -5,12 +5,17 @@ session_start();
 
 function registerUser($username,$email,$password,$c_password){
 
-
-    if ($password != $c_password){
+    if(emailExisted($email)){
+        echo "Email already exists";
+    }
+    elseif($password != $c_password){
         echo "رمز عبور خالی است یا مطابقت ندارد!";
     }else{
+
+        $hashPassword = password_hash($password,PASSWORD_DEFAULT);
+
         $file = fopen("../Users.txt" , "a");
-        fwrite($file,"username:$username:email:$email:password:$password\n");
+        fwrite($file,"username:$username:email:$email:password:$hashPassword\n");
         fclose($file);
 
         setUserSession($username);
@@ -20,7 +25,7 @@ function registerUser($username,$email,$password,$c_password){
 
 }
 
-function loginUser($email , $password )
+function loginUser($email , $password ): bool
 {
     $users = file("../Users.txt",FILE_IGNORE_NEW_LINES);
 
@@ -30,9 +35,26 @@ function loginUser($email , $password )
         $storedEmail = $fileds[3];
         $storedPassword = $fileds[5];
 
-        if ($storedEmail == $email && $storedPassword == $password){
+        if ($storedEmail == $email && password_verify($password,$storedPassword) ){
             $username = $fileds[1];
             setUserSession($username);
+            return true;
+        }
+
+    }
+    return false;
+
+}
+
+function emailExisted(String $email): bool
+{
+    $users = file("../Users.txt",FILE_IGNORE_NEW_LINES);
+
+    foreach ($users as $user){
+        $fileds = explode(":" , $user);
+        $storedEmail = $fileds[3];
+
+        if ($storedEmail == $email){
             return true;
         }
 
--- functions/helper.php
+++ functions/helper.php
@@ -1,12 +1,13 @@
 <?php
 
-function setUserSession($username){
+function setUserSession($username): void
+{
     $expire = time() + (30 * 24 * 60 * 60 );
     $_SESSION["username"] = $username;
     setcookie("session_id" , session_id() , $expire , "/");
 }
 
-function redirect($route)
+function redirect($route): void
 {
     header("Location:" . $route);
 }
